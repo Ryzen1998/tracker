b8df444803eb054522ba7af1c7cd2bc8b20ec104
Ryzen1998 <apexashwin@gmail.com>
2022-09-30 21:21:00 +0300

    added avatar holder with temp image, file upload will be implemented later

--- src/Components/Chat/ReceiverBubble.jsx
+++ src/Components/Chat/ReceiverBubble.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Box } from "@mui/material";
+import Avatar from '@mui/material/Avatar';
 
 const ReceiverBubble = (props) => {
   return (
     <>
-      <Box className="speech left">
-        <p>{props.message}</p>
+      <Box className="rcbubble-holder">
+        <Box>
+          <Avatar alt="profileimage" src="https://img.freepik.com/free-photo/beautiful-brun
+          ette-serious-girl-portrait_23-2148176372.jpg?w=826&t=st=1664561550~exp=16645
+          62150~hmac=09707c183fd0a1dff8d9bd5dafc90dd5a3cbeaf39598bf227fe2d99e6f258f0a"/>
+        </Box>
+      <Box className="speech bottom">
+       <p>{props.message}</p>
+      </Box>
       </Box>
     </>
   );
--- src/Components/Chat/SenderBubble.jsx
+++ src/Components/Chat/SenderBubble.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Box } from "@mui/system";
+import Avatar from '@mui/material/Avatar';
 
 const SenderBubble = (props) => {
   return (
     <>
-      <Box className="speechsender right">
-        <p>{props.message}</p>
+      <Box className="rcbubble-holder">
+        <Box className="speechsender right">
+          <p>{props.message}</p>
+        </Box>
+        <Box>
+          <Avatar alt="profileimage" src="https://img.freepik.com/free-photo/beautiful-brun
+          ette-serious-girl-portrait_23-2148176372.jpg?w=826&t=st=1664561550~exp=16645
+          62150~hmac=09707c183fd0a1dff8d9bd5dafc90dd5a3cbeaf39598bf227fe2d99e6f258f0a"/>
+        </Box>
       </Box>
     </>
   );
